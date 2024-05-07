3f82678106d143c93658f9e29f386e9ceda86bc3
host <taijomi17@gmail.com>
2024-05-07 19:22:53 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_app_course/presentation/screens/counter/counter_function_scren.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const CounterFunctionScreen(),
+    );
+  }
+}
